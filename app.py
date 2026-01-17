28083062eedec8c0f1b7071362b44653247d16c5
Jonny84-max <jonnybest0684@gmail.com>
2026-01-17 16:17:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ from sklearn.model_selection import train_test_split
 # 1. Create dataset
 # ---------------------------
 data = {
-    'Temperature': [75, 80, 90, 70, 85, 95, 65, 78, 92, 88],
+    'Temperature': [77, 80, 90, 70, 85, 95, 65, 79, 92, 88],
     'Vibration': [0.5, 0.7, 0.9, 0.4, 0.8, 1.0, 0.3, 0.6, 0.95, 0.85],
-    'Pressure': [30, 35, 40, 28, 38, 42, 25, 33, 41, 39],
-    'MaintenanceNeeded': [0, 0, 1, 0, 1, 1, 0, 0, 1, 1]
+    'Pressure': [30, 35, 40, 24, 38, 42, 22, 33, 41, 39],
+    'MaintenanceNeeded': [1, 1, 1, 0, 1, 1, 0, 1, 1, 1]
 }
 
 df = pd.DataFrame(data)
@@ -38,7 +38,7 @@ model.fit(X_train, y_train)
 def decision_system(temp, vib, pres):
     # Rule-based thresholds
     rule_maintenance = False
-    if temp >= 90 or vib >= 0.9 or pres >= 40:
+    if temp >= 85 or vib >= 0.8 or pres >= 42:
         rule_maintenance = True
 
     # AI prediction
@@ -60,9 +60,9 @@ def decision_system(temp, vib, pres):
 # ---------------------------
 st.title("Predictive Maintenance System")
 
-temp = st.number_input("Temperature", value=87.0)
-vib = st.number_input("Vibration", value=0.7)
-pres = st.number_input("Pressure", value=39.0)
+temp = st.number_input("Temperature", value=80.0)
+vib = st.number_input("Vibration", value=0.5)
+pres = st.number_input("Pressure", value=30.0)
 
 if st.button("Check Maintenance"):
     final_decision, prob, rule_flag = decision_system(temp, vib, pres)
